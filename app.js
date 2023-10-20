3b2e36189f564785179f566c7758297cfd0089d2
Kai-Shleymovich <124058665+Kai-Shleymovich@users.noreply.github.com>
2023-10-20 13:18:36 -0400

    kinda start

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
-const DOMSelectors={
-    
+const DOMSelectors = {
+  button: document.getElementById("btn"),
+};
+function background() {
+ if (input='red')
+ document.body.style.backgroundColor = "red";
 }
