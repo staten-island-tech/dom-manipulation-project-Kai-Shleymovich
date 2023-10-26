ec2328ac2109815032b552c42ea93fdfbb738591
Kai-Shleymovich <124058665+Kai-Shleymovich@users.noreply.github.com>
2023-10-26 13:18:34 -0400

    blah

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
 const DOMSelectors = {
   form: document.getElementById('form'),
   nameinput: document.getElementById('color_name'),
-  hexinput: document.getElementById('colors')
+  hexinput: document.getElementById('colors'),
+  selector: document.querySelector(".flex-wrapper"),
+  h2s : document.querySelectorAll("h2"),
 };
 DOMSelectors.form.addEventListener('submit',function(e){
   e.preventDefault()
-document.body.style.backgroundColor = DOMSelectors.input.value
+  DOMSelectors.selector.insertAdjacentHTML(
+    "beforeend",
+    `      <div class="card">
+    <h2>${DOMSelectors.nameinput.value}</h2>
+    <div class="background" style="background-color:${DOMSelectors.hexinput.value}"></div>
+    <button class="delbutton">Delete</button>
+  </div>`,
+  );
+  const delbuttons = document.querySelectorAll('.delbutton')
+  console.log(delbuttons)
+  //foreach button add event listener to clear that instance of the card. and fix css
 });
-/* function background() {
- if (input='red')
- document.body.style.backgroundColor = "red";
- if (input='green')
- document.body.style.backgroundColor = "green";
-}
- */
