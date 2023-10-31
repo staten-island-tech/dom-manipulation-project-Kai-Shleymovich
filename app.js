30cf5c9f7bde0c36f39ba27e49046d3f314d3da8
Kai-Shleymovich <124058665+Kai-Shleymovich@users.noreply.github.com>
2023-10-31 13:04:16 -0400

    done other than css

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const DOMSelectors = {
   form: document.getElementById('form'),
   nameinput: document.getElementById('color_name'),
+  person_name_input: document.getElementById('name'),
   hexinput: document.getElementById('colors'),
   selector: document.querySelector(".flex-wrapper"),
   h2s : document.querySelectorAll("h2"),
@@ -12,10 +13,14 @@ DOMSelectors.form.addEventListener('submit',function(e){
     `      <div class="card">
     <h2>${DOMSelectors.nameinput.value}</h2>
     <div class="background" style="background-color:${DOMSelectors.hexinput.value}"></div>
+    <p>${DOMSelectors.person_name_input.value}</p>
+    
     <button class="delbutton">Delete</button>
   </div>`,
   );
-  const delbuttons = document.querySelectorAll('.delbutton')
-  console.log(delbuttons)
-  //foreach button add event listener to clear that instance of the card. and fix css
 });
+DOMSelectors.selector.addEventListener("click",function(e){
+  if(e.target.classList.contains('delbutton')){
+    e.target.parentElement.remove();
+  }
+})
